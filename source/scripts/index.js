41067d11385361c7e202014aa6c5e2201af1335f
TanyaEremina <158171587+TanyaEremina@users.noreply.github.com>
2024-05-19 10:48:07 +0800

    Добавляет сетки страниц (#9)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navheader = document.querySelector('.header-nav');
+const navToggle = document.querySelector('.header-nav__toggle');
+
+navheader.classList.remove('header-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navheader.classList.contains('header-nav--closed')) {
+    navheader.classList.remove('header-nav--closed');
+    navheader.classList.add('header-nav--opened');
+  } else {
+    navheader.classList.add('header-nav--closed');
+    navheader.classList.remove('header-nav--opened');
+  }
+});/* в этот файл добавляет скрипты*/
